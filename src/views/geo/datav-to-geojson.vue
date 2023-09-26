222984f439267c33953fae73223b892a4e5950b7
yujinpan <1192878390@qq.com>
2023-09-26 17:32:27 +0800

    feat: add datav-to-geojson more options

--- src/views/geo/datav-to-geojson.vue
+++ src/views/geo/datav-to-geojson.vue
@@ -12,11 +12,12 @@
       multiple
     />
 
+    <h3 class="margin-top-medium">Description</h3>
     <div class="margin-top-base">
       <a
         target="_blank"
         href="http://datav.aliyun.com/portal/school/atlas/level_generator"
-        >DataVJSON（导出层级(开放格式）</a
+        >DataVJSON（层级.json(开放格式)）</a
       >:
       <pre><code>{ name: string; payload: TopoJSON; children: {...} }</code></pre>
     </div>
@@ -24,27 +25,69 @@
       GeoJSON:
       <pre><code>{ type: 'FeatureCollection', features: [] }</code></pre>
     </div>
+    <h3 class="margin-top-medium">Options</h3>
+    <ul>
+      <li class="margin-top-base">
+        <span>Keep Area Tree</span>&nbsp;&nbsp;
+        <el-switch v-model="keepAreaTree" />
+        <pre><code>[{ code, name, children, geojson }]</code></pre>
+        <ul v-if="keepAreaTree">
+          <li class="margin-top-base">
+            <span>Save Level</span>&nbsp;&nbsp;
+            <ElRadioGroup v-model="saveLevel">
+              <ElRadioButton
+                v-for="item in AreaLevels"
+                :key="item"
+                :value="item"
+                :label="item"
+              />
+            </ElRadioGroup>
+            <pre class="margin-top-base">
+- {{ AreaLevel.COUNTRY }}
+  - {{ AreaLevel.PROVINCE }}
+    - {{ AreaLevel.CITY }}
+      - {{ AreaLevel.DISTRICT }}
+            </pre>
+          </li>
+        </ul>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
-import { ElButton } from 'element-plus';
+import { ElButton, ElSwitch, ElRadioGroup, ElRadioButton } from 'element-plus';
 import { defineComponent } from 'vue';
 
 import { downloadByContent, readJSONFile } from '@/utils/file';
 import type { DataVJSON } from '@/views/geo/utils';
-import { datavToGeoJSON } from '@/views/geo/utils';
+import {
+  AreaLevel,
+  AreaLevels,
+  datavToAreaTree,
+  datavToGeoJSON,
+} from '@/views/geo/utils';
 
 export default defineComponent({
   components: {
     ElButton,
+    ElSwitch,
+    ElRadioGroup,
+    ElRadioButton,
   },
   data() {
     return {
       loading: false,
+      keepAreaTree: false,
+      saveLevel: AreaLevel.DISTRICT,
+      AreaLevels,
+      AreaLevel,
     };
   },
   methods: {
+    downloadJson(json, name) {
+      downloadByContent(JSON.stringify(json), `${name}.json`);
+    },
     uploadFile() {
       const files = Array.from(this.$refs.input.files);
       this.$refs.input.value = '';
@@ -53,10 +96,17 @@ export default defineComponent({
       return Promise.all(
         files.map(async (file) => {
           const json = (await readJSONFile(file)) as DataVJSON;
-          const geojson = datavToGeoJSON(json);
 
-          if (geojson.features.length) {
-            downloadByContent(JSON.stringify(geojson), `${json.name}.json`);
+          if (this.keepAreaTree) {
+            const areaTreeJson = datavToAreaTree(json, this.saveLevel);
+
+            this.downloadJson(areaTreeJson, json.name);
+          } else {
+            const geojson = datavToGeoJSON(json);
+
+            if (geojson.features.length) {
+              this.downloadJson(geojson, json.name);
+            }
           }
         }),
       ).finally(() => (this.loading = false));
--- src/views/geo/utils.ts
+++ src/views/geo/utils.ts
@@ -1,4 +1,5 @@
 import { GeoJSON, TopoJSON } from 'ol/format';
+import { treeFilter, treeMap } from 'operation-tree-node';
 
 import type { GeoJSONFeatureCollection } from 'ol/format/GeoJSON';
 import type { TopoJSONTopology } from 'ol/format/TopoJSON';
@@ -15,15 +16,9 @@ export function datavToGeoJSON(json: DataVJSON) {
     features: [],
   };
 
-  const geojson = new GeoJSON();
-  const topojson = new TopoJSON();
-
   const appendFeature = (json: DataVJSON) => {
     try {
-      geojsonData.features.push(
-        ...geojson.writeFeaturesObject(topojson.readFeatures(json.payload))
-          .features,
-      );
+      geojsonData.features.push(...topojsonToGeoJSON(json.payload).features);
     } catch (e) {
       // eslint-disable-next-line no-console
       console.log(e);
@@ -35,4 +30,90 @@ export function datavToGeoJSON(json: DataVJSON) {
   appendFeature(json);
 
   return geojsonData;
+}
+
+const geojson = new GeoJSON();
+const topojson = new TopoJSON();
+
+export function topojsonToGeoJSON(
+  topo: TopoJSONTopology,
+): GeoJSONFeatureCollection {
+  return geojson.writeFeaturesObject(topojson.readFeatures(topo));
+}
+
+export enum AreaLevel {
+  COUNTRY = 'country',
+  PROVINCE = 'province',
+  CITY = 'city',
+  DISTRICT = 'district',
+}
+
+export const AreaLevels = [
+  AreaLevel.COUNTRY,
+  AreaLevel.PROVINCE,
+  AreaLevel.CITY,
+  AreaLevel.DISTRICT,
+];
+
+export type Properties = {
+  adcode: number;
+  center: number[];
+  centroid: number[];
+  level: AreaLevel;
+};
+
+export function formatProperties(props: Properties): Properties {
+  const { adcode, center, centroid, level } = props;
+
+  return {
+    adcode,
+    center,
+    centroid,
+    level,
+  } as Properties;
+}
+
+export type AreaTreeNode = {
+  code: number;
+  name: string;
+  children: AreaTreeNode[];
+  geojson: GeoJSONFeatureCollection;
+};
+
+function getLevelIndex(level: AreaLevel) {
+  return AreaLevels.indexOf(level);
+}
+
+function inLevel(sourceLevel: AreaLevel, targetLevel: AreaLevel) {
+  return getLevelIndex(sourceLevel) >= getLevelIndex(targetLevel);
+}
+
+export function datavToAreaTree(
+  json: DataVJSON,
+  level = AreaLevel.DISTRICT,
+): AreaTreeNode[] {
+  const treeData = treeMap([json], (node) => ({
+    ...node,
+    children: Object.values(node.children),
+  }));
+
+  const result = treeMap(treeData, (node) => {
+    const geojson = topojsonToGeoJSON(node.payload).features[0];
+    geojson.properties = formatProperties(geojson.properties);
+
+    return {
+      code: geojson.properties.adcode,
+      name: node.name,
+      geojson,
+      children: node.children as any,
+    } as AreaTreeNode;
+  });
+
+  if (level !== AreaLevel.DISTRICT) {
+    return treeFilter(result, (node) =>
+      inLevel(level, node.geojson.properties.level),
+    );
+  } else {
+    return result;
+  }
 }
